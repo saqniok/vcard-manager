d947b5e2368168014127b20e54b4bbb3e3ac4eb3
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-07-01 15:10:51 +0200

    SystemConsole: Method explanations: - Console.WriteLine - Console.Write - Console.ReadLine

--- VCardManager.CLI/SystemConsole.cs
+++ VCardManager.CLI/SystemConsole.cs
@@ -4,7 +4,10 @@ namespace VCardManager.CLI;
 
 public class SystemConsole : IConsole
 {
-    public void WriteLine(string message) => Console.WriteLine(message);
-    public void Write(string message) => Console.Write(message);
-    public string ReadLine() => Console.ReadLine() ?? string.Empty;
+    public void WriteLine(string message) => Console.WriteLine(message);        // This method outputs the specified string to the console and then moves the cursor to the new line
+    public void Write(string message) => Console.Write(message);                // This method outputs the specified string to the console, BUT does not move the cursor to a new line,
+                                                                                // allowing the following output to be displayed on the same line
+    public string ReadLine() => Console.ReadLine() ?? string.Empty;             // Reads a single line of text entered by the user from the console (before pressing Enter).
+                                                                                // It returns string, but if it's null `??` then returns string (string.Empty)
 }
+
