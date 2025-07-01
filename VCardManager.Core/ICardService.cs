fe075608fffcfe31cda42ace08bf120bc0c48b09
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-07-01 15:16:37 +0200

    ICardService: - IEnumerable<T> explanation - Why this interface is importa and hes functionality in future

--- VCardManager.Core/ICardService.cs
+++ VCardManager.Core/ICardService.cs
@@ -10,3 +10,14 @@ namespace VCardManager.Core
         IEnumerable<VCard> FindByName(string name);
     }
 }
+
+/*
+    #   `IEnumerable<VCard>`    - type of the returned value. IEnumerable<T> represents a collection of
+                                elements of type T (in this case VCard) that can be enumerated (e.g., in a foreach loop). 
+                                It specifies that the method will return a list or set of all VCard objects
+            
+    #   `ICardService`          - is a critical component in an architecture based on OOP and clean code principles. 
+                                It defines the contract for the core business logic of your VCardManager, 
+                                abstracting away the implementation details. This allows your application to be more 
+                                modular, testable, and flexible to changes in the future
+*/
